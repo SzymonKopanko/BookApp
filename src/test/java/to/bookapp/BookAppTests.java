96a56f1d3636bcd343206167a89ee697ccc83d98
SzymonKopanko <skopanko320@gmail.com>
2023-11-21 22:20:37 +0100

    Initial commit

---
+++ src/test/java/to/bookapp/BookAppTests.java
@@ -0,0 +1,13 @@
+package to.bookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
